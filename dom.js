916f200cea4f3e41097f4754316d6342f9d7a3dc
atabhinav7 <atabhinav7@gmail.com>
2023-03-06 20:17:23 +0530

    first commit

--- dom.js
+++ dom.js
@@ -31,15 +31,27 @@
 
 // GETELEMENTSBYCLASSNAME //
 
-var items = document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// //items[1].textContent = 'Hello 2';
-// //items[1].style.fontweight = 'bold';
-items[1].style.backgroundColor = 'yellow';
-// //items.style.backgroundColor = 'red';
-items[2].style.backgroundColor = 'green';
- for ( i = 0; i < items.length ; i++){
-   items[i].style.fontWeight = 'bold';
- }
+// var items = document.getElementsByClassName('list-group-item');
+// // console.log(items);
+// // console.log(items[1]);
+// // //items[1].textContent = 'Hello 2';
+// // //items[1].style.fontweight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+// // //items.style.backgroundColor = 'red';
+// items[2].style.backgroundColor = 'green';
+//  for ( i = 0; i < items.length ; i++){
+//    items[i].style.fontWeight = 'bold';
+//  }
 
+//GETELEMENTSBYTAGNAME //
+var li  = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontweight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+
+
+ for ( i = 0; i < li.length ; i++){
+   li[i].style.backgroundColor = '#f4f4f4';
+ }
